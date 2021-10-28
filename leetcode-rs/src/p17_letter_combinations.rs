29735a032ecc11db9113711f64cc8d9c8203e45f
haotian <ritou11@gmail.com>
2021-10-29 00:48:08 +0800

    p17 another format

--- leetcode-rs/src/p17_letter_combinations.rs
+++ leetcode-rs/src/p17_letter_combinations.rs
@@ -18,20 +18,25 @@ impl Solution {
         ava.insert('8' as u8, vec!['t', 'u', 'v']);
         ava.insert('9' as u8, vec!['w', 'x', 'y', 'z']);
 
-        let mut stack: Vec<(String, usize)> = Vec::new();
-        stack.push((String::from(""), 0));
+        let mut stack: Vec<(usize, usize)> = Vec::new();
+        let mut s = String::from("");
+        stack.push((0, 0));
         while stack.len() > 0 {
-            let mut s = stack.pop().unwrap();
-            if s.0.len() == n {
-                res.push(s.0.clone());
+            let state = stack.pop().unwrap();
+            if state.0 == n {
+                res.push(s.clone());
+                s.pop();
             } else {
-                let pos = s.0.len();
-                let idx = s.1;
+                let pos = state.0;
+                let idx = state.1;
                 let c = digits[pos];
-                if idx >= ava.get(&c).unwrap().len() { continue; }
-                stack.push((s.0.clone(), idx + 1));
-                s.0.push(ava.get(&c).unwrap()[idx]);
-                stack.push((s.0, 0));
+                if idx >= ava.get(&c).unwrap().len() {
+                    s.pop();
+                    continue;
+                }
+                stack.push((state.0, idx + 1));
+                s.push(ava.get(&c).unwrap()[idx]);
+                stack.push((state.0 + 1, 0));
             }
         }
         res
