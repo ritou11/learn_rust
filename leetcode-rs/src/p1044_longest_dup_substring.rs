f14f2682ebf0d85e3941f46dd6c58851a9804212
ritou11 <ritou11@gmail.com>
2021-11-01 20:24:56 +0800

    WIP: 1044 correct but slow

--- leetcode-rs/src/p1044_longest_dup_substring.rs
+++ leetcode-rs/src/p1044_longest_dup_substring.rs
@@ -1,52 +1,71 @@
 use crate::Solution;
+use std::collections::HashMap;
 
 impl Solution {
     pub fn longest_dup_substring(s: String) -> String {
         if s.len() <= 1 {
             return String::from("");
         }
-        let mut tails: Vec<(usize, Vec<usize>)> = Vec::new();
-        tails.push((0, vec![]));
+        // let mut tails: Vec<(usize, Vec<usize>)> = Vec::new();
+        // tails.push((0, vec![]));
         let bytes = s.into_bytes();
+
+        let mut know: HashMap<u8, Vec<usize>> = HashMap::new();
+        let mut poss = vec![];
+        for k in 0..bytes.len() {
+            (*know.entry(bytes[k]).or_insert(Vec::<usize>::new())).push(k);
+            poss.push(30000);
+        }
+
+        let mut memory = HashMap::new();
         let mut max_len = 0;
         let mut max_pos = 0;
-        for k in 1..bytes.len() {
-            let c = bytes[k];
-            let mut lst: Vec<usize> = vec![];
-            let i = tails[k - 1].0;
-            if i == 0 {
-                for p in 0..k {
-                    if bytes[p] == c {
-                        lst.push(p);
+        fn search(bytes: &Vec<u8>, k: usize, i: usize, memory: &mut HashMap<(usize, usize), Vec<usize>>, know: &HashMap<u8, Vec<usize>>, poss: &mut Vec<usize>) -> Vec<usize> {
+            if i > poss[k] || k < i { return vec![]; }
+            match memory.get(&(k, i)) {
+                Some(res) => { res.to_vec() },
+                None => {
+                    println!("{:?}, {:?}", k, i);
+                    let c = bytes[k];
+                    let mut lst: Vec<usize> = vec![];
+                    if i == 1 {
+                        match know.get(&c) {
+                            Some(ava) => {
+                                for p in ava {
+                                    if *p < k {
+                                        lst.push(*p);
+                                    } else {
+                                        break;
+                                    }
+                                }
+                            },
+                            _ => {}
+                        }
+                    } else {
+                        let bf = search(bytes, k - 1, i - 1, memory, know, poss);
+                        for p in bf {
+                            if bytes[p + i - 1] == c {
+                                lst.push(p);
+                            }
+                        }
                     }
-                }
-            } else {
-                for p in &tails[k - 1].1 {
-                    if bytes[p + i] == c {
-                        lst.push(*p);
-                    }
-                }
-            }
-            if lst.len() > 0 {
-                if i + 1 > max_len {
-                    max_len = i + 1;
-                    max_pos = lst[0];
-                }
-                tails.push((i + 1, lst));
-            } else {
-                for p in 0..k {
-                    if bytes[p] == c {
-                        lst.push(p);
-                    }
-                }
-                if lst.len() > 0 {
-                    tails.push((1, lst));
-                } else {
-                    tails.push((0, lst));
+                    memory.insert((k, i), lst.clone());
+                    lst
                 }
             }
         }
-        println!("{:?}", tails);
+        for k in 1..bytes.len() {
+            let mut lst: Vec<usize> = vec![];
+            let mut i = max_len + 1;
+            lst = search(&bytes, k, i, &mut memory, &know, &mut poss);
+            while lst.len() > 0 {
+                max_len = i;
+                max_pos = lst[0];
+                i += 1;
+                lst = search(&bytes, k, i, &mut memory, &know, &mut poss);
+            }
+            poss[k] = i - 1;
+        }
         if max_len > 0 {
             String::from_utf8(bytes[max_pos..max_pos + max_len].to_vec()).unwrap()
         } else {
