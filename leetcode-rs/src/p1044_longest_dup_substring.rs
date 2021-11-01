b52463e69369fea90cedd1b945980781c5e6ca7f
ritou11 <ritou11@gmail.com>
2021-11-01 21:43:37 +0800

    1044 sliding window

--- leetcode-rs/src/p1044_longest_dup_substring.rs
+++ leetcode-rs/src/p1044_longest_dup_substring.rs
@@ -1,93 +1,29 @@
 use crate::Solution;
-use std::collections::HashMap;
 
 impl Solution {
     pub fn longest_dup_substring(s: String) -> String {
-        if s.len() <= 1 {
-            return String::from("");
-        }
-        let bytes = s.into_bytes();
         let mut max_len = 0;
         let mut max_pos = 0;
 
-        let mut know: HashMap<u8, Vec<(usize, usize)>> = HashMap::new();
-        for k in 1..bytes.len() {
-            let before = know.get(&bytes[k - 1]);
-            if before.is_some() {
-                let lst = before.unwrap().clone();
-                let ctails = know.entry(bytes[k]).or_insert(Vec::<(usize, usize)>::new());
-                for pair in lst {
-                    if pair.0 + pair.1 != k && bytes[pair.0 + pair.1] == bytes[k] {
-                        ctails.push((pair.0, pair.1 + 1));
-                        if pair.1 + 1 > max_len {
-                            max_len = pair.1 + 1;
-                            max_pos = pair.0;
-                        }
-                    }
-                }
-                ctails.push((k, 1));
-            } else {
-                let ctails = know.entry(bytes[k]).or_insert(Vec::<(usize, usize)>::new());
-                ctails.push((k, 1));
-            }
+        let str = s.as_bytes();
+        if str.iter().all(|&value| value == str[0]) {
+            return s[1..s.len()].to_string();
         }
 
-        if max_len > 0 {
-            String::from_utf8(bytes[max_pos..max_pos + max_len].to_vec()).unwrap()
-        } else {
-            String::from("")
-        }
-    }
-}
-pub fn longest_dup_substring(s: String) -> String {
-    if s.len() <= 1 {
-        return String::from("");
-    }
-    let mut tails: Vec<(usize, Vec<usize>)> = Vec::new();
-    tails.push((0, vec![]));
-    let bytes = s.into_bytes();
-    let mut max_len = 0;
-    let mut max_pos = 0;
-    for k in 1..bytes.len() {
-        let c = bytes[k];
-        let mut lst: Vec<usize> = vec![];
-        let i = tails[k - 1].0;
-        if i == 0 {
-            for p in 0..k {
-                if bytes[p] == c {
-                    lst.push(p);
-                }
+        let mut left = 0;
+        for end in 1..s.len() {
+            if max_len >= (end - left) {
+                break;
             }
-        } else {
-            for p in &tails[k - 1].1 {
-                if bytes[p + i] == c {
-                    lst.push(*p);
-                }
-            }
-        }
-        if lst.len() > 0 {
-            if i + 1 > max_len {
-                max_len = i + 1;
-                max_pos = lst[0];
-            }
-            tails.push((i + 1, lst));
-        } else {
-            for p in 0..k {
-                if bytes[p] == c {
-                    lst.push(p);
-                }
-            }
-            if lst.len() > 0 {
-                tails.push((1, lst));
+            let curr = &s[left..end];
+
+            if s[(left + 1)..s.len()].contains(curr) && curr.len() > max_len {
+                max_pos = left;
+                max_len = curr.len();
             } else {
-                tails.push((0, lst));
+                left += 1;
             }
         }
-    }
-    // println!("{:?}", tails);
-    if max_len > 0 {
-        String::from_utf8(bytes[max_pos..max_pos + max_len].to_vec()).unwrap()
-    } else {
-        String::from("")
+        return s[max_pos..max_pos + max_len].to_string();
     }
 }
